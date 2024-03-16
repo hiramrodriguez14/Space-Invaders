107b51ce54508097079c6ace54932a51f94d7d6e
hiramrodriguez14 <hiram.rodriguez14@upr.edu>
2024-03-16 18:44:15 -0400

    Fixed hit box, position and shooting point of Space Station; added spiral shooting but it is incomplete(it shoots very slow and poor but the movement and position is right)

--- src/Ships/EnemyShips/Bosses/SpaceStation.cpp
+++ src/Ships/EnemyShips/Bosses/SpaceStation.cpp
@@ -1,10 +1,10 @@
 #include "SpaceStation.h"
 
-SpaceStation::SpaceStation(int xpos, int ypos, string name) : Boss(ofGetWidth()/2, ofGetHeight()/2 ,0.5, 250, name) {
+SpaceStation::SpaceStation(int xpos, int ypos, string name) : Boss(ofGetWidth()/2 - 60, ofGetHeight()/2 - 60   ,0.5, 250, name) {
     enemyShipSprite.load("ShipModels/Space_Station.png");
-    enemyHitBox = new HitBox(pos.x, pos.y - 30, enemyShipSprite.getWidth() * 0.05, enemyShipSprite.getHeight() * 0.075);
+    enemyHitBox = new HitBox(ofGetWidth()/2-70, ofGetHeight()/2-65, enemyShipSprite.getWidth() * 0.3, enemyShipSprite.getHeight() * 0.38);
     
-    shootingPoint = ofPoint(pos.x + enemyHitBox->box.getWidth(), pos.y + enemyHitBox->box.getHeight() / 2);
+    shootingPoint = ofPoint(pos.x + enemyHitBox->box.getWidth()/2 , pos.y + enemyHitBox->box.getHeight() / 2 +1 );
     
 
 }
@@ -45,23 +45,15 @@ void SpaceStation::draw() {
 
 
 void SpaceStation::shoot() {
-    const int numBullets = 36; // Increase the number of bullets for a smoother spiral
-    const float angleStep = 360.0 / numBullets; // Ensure a full circle is covered
+    
 
-    for (int i = 0; i < numBullets; ++i) {
-        // Calculate the angle for each bullet, incorporating the shootAngleOffset
-        float angle = i * angleStep + shootAngleOffset;
-
-        // Create and initialize the bullet at the boss's position with the given angle
-        // Note: The angle is directly used in the Projectiles constructor
-        Projectiles bullet(shootingPoint, angle);
-        bullet.setSpeed(3);
-        bullet.setColors(ofColor::red, ofColor::blue); // Set desired colors
-        enemyBullets.push_back(bullet);
-    }   
-
+ float angle = shootAngleOffset;
+    Projectiles bullet(shootingPoint, angle);
+    bullet.setSpeed(10);
+    bullet.setColors(ofColor::red, ofColor::blue);
+    enemyBullets.push_back(bullet);
     // Increment the shootAngleOffset for the next call to shoot() to continue the spiral
-    shootAngleOffset += 10.0; // Adjust this value for the desired spiral rotation speed
+    shootAngleOffset += 30.0; // Adjust this value for the desired spiral rotation speed
 
     // Optional: Reset shootAngleOffset to keep it within 0-360 degrees to avoid overflow
     if(shootAngleOffset >= 360.0) {
@@ -70,3 +62,4 @@ void SpaceStation::shoot() {
 
     SoundManager::playSong("bulletSound", false);
 }
+
